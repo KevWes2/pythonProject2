9de10cc22ca166d110b319aa7cf7ceb60d348a8b
kiran weston <kiranweston@kirans-MacBook-Air-2.local>
2022-11-14 13:38:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#need to import csv, can read them out of pandas
+
+pd.read_csv('/Users/kiranweston/Documents/University/Year 4/Effects of morphing wing morphology on bird and bird-inspired flight dynamics and control/Flight data/TeST.csv')
